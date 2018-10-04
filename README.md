0669356114946a05f474e9f299d13cd69488d10a
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-10-04 21:25:24 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODO-Backend
+
+Author : Sylvain Julmy
--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-# global variable
-conn = sqlite3.connect('todo_backend.sqlite')
-c = conn.cursor()
-c.execute(f'''select id,title,completed,t_order from todos''')
-data = c.fetchall()
-
-print(data)
